8ffcd4110546c863a004192039ddc2cf68b3f1d9
Ciara Weldon <cweldon@ncat.edu>
2025-10-03 20:01:12 -0400

    Challenge 2: Prime checker - for loop for known range

--- comp163-assignment-5.py
+++ comp163-assignment-5.py
@@ -11,3 +11,15 @@ while current_number !=1:
     step_count += 1
 print(current_number)
 print("Steps:", step_count)
+
+print("=== Challenge 2: Prime Number Checker ===")
+n = int(input("Enter a number:"))
+print(f" Testing divisors from 2 to {n-1}...")
+is_prime = True
+for i in range(2, n):
+    if n % i == 0:#divides my code evenly
+          print(f"{n} is not prime (divisible by {i})")
+          is_prime = False
+          break
+if is_prime:
+    print(f"{n} is prime!")
